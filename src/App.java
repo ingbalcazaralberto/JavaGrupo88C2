217796a1d2ea0c48eb3e3783457c96d0572439d2
ingbalcazaralberto <balcazaralberto@gmail.com>
2021-07-12 18:40:17 -0500

    Java lang lesson

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
+
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private double peso;
+    private char sexo;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int resultado=45+12;
+
+        resultado=3;
+
+        if (resultado>0){
+
+        } else if(resultado==0){
+
+        }
+
+
+
+      
     }
 }
---
+++ src/model/wallet.java
@@ -0,0 +1,7 @@
+package model;
+
+public class wallet {
+    public static final int capacidadMax=1000000;
+    private int saldo;    
+    
+}
