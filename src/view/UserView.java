4de6aa987792e9476af0541012fc5657a83e414f
ingbalcazaralberto <balcazaralberto@gmail.com>
2021-07-14 18:27:00 -0500

    cambios clase 08/07/2021

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -5,8 +5,12 @@ import model.Owner;
 public class UserView {
     public static void main(String[] args) {
         Owner cliente1 = new Owner("Tiqui");
-        System.out.println(cliente1.getNombre());
         Owner cliente2 = new Owner("Alberto");
-        System.out.println(cliente2.getNombre());        
+        System.out.println(cliente1.getNombre());
+        System.out.println(cliente2.getNombre());
+        String response = cliente1.getWallet().saveMoney(100000);
+        System.out.println(response);  
+        System.out.println(cliente1.getWallet().getSaldo());       
+        System.out.println(cliente2.getWallet().getSaldo());
     }
 }
